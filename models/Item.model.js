d8514b99eb7a7ab34cc595b76d4f7034cedfa934
Andy Servet Jouve <aservetjouve@gmail.com>
2020-06-24 12:23:46 +0200

    add item model

---
+++ models/Item.model.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const itemSchema = new Schema(
+  {
+    name : {
+      type: String, 
+      required: true
+    },
+    type : {
+      type: String, 
+      required: true
+    },
+    aspect : {
+      type: String, 
+      required: true
+    },
+    swappableWith : {
+      type: String, 
+      required: true
+    },
+    owner : {
+      type: Schema.Types.ObjectId, 
+      ref: 'User'}
+  }
+);
+
+module.exports = model('Item', itemSchema)
