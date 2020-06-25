46048b57d3ba347d751ebbcbb0765276d0ed2579
Andy Servet Jouve <aservetjouve@gmail.com>
2020-06-25 14:41:07 +0200

    refactor the matching logic

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,63 +1,52 @@
 const express = require("express");
 const router = express.Router();
 
-const TransactionModel = require('../models/Transaction.model');
-const ItemModel = require('../models/Item.model')
+const TransactionModel = require("../models/Transaction.model");
+const ItemModel = require("../models/Item.model");
 
 /* Check if the user is logged in*/
 const { isLoggedIn } = require("../helpers/auth-helper");
 
-router.post("/:id", isLoggedIn, (req, res) => {
-	let userA = req.session.currentUser._id;
-	let itemUserA = "";
-	let userB = "";
-	let itemUserB = req.params.id;
-	ItemModel.find({ owner: userA }).then((result) => {
-		itemUserA = result[0]._id;
-		ItemModel.find({ _id: itemUserB }).then((result) => {
-			userB = result[0].owner;
+router.post("/:myItem/:otherItem", isLoggedIn, (req, res) => {
+	let itemUserA = req.params.myItem;
+	let itemUserB = req.params.otherItem;
+	TransactionModel.find({
+		itemUserA: itemUserA,
+		itemUserB: itemUserB,
+	}).then((result) => {
+		/*CHECK IF TRANSACTION ALREADY EXISTS*/
+		if (!result.length) {
 			TransactionModel.find({
-				userA: userA,
-				userB: userB,
-				itemUserA: itemUserA,
-				itemUserB: itemUserB,
-			})
-				.then((result) => {
-					if (!result.length){
-                        TransactionModel.find({
-                            userA: userB,
-                            userB: userA,
-                            itemUserA: itemUserB,
-                            itemUserB: itemUserA,
-                        })
-                            .then((result) => {
-                                if (!result.length){
-                                    TransactionModel.create({
-                                        userA: userA,
-                                        itemUserA: itemUserA,
-                                        userB: userB,
-                                        itemUserB: itemUserB,
-                                        oneUserAgreed: true,
-                                    })
-                                        .then((response) => {
-                                            res.status(200).json(response);
-                                        })
-                                        .catch((err) => {
-                                            res.status(500).json({
-                                                error: "Something went wrong",
-                                                message: err,
-                                            });
-                                        });
-                                } else {
-                                    res.send("It's a match")
-                                }
-                                }) 
-                                
-                    } else {
-                        res.send('Already created')
-                    }
-				})					
-		});
+				itemUserA: itemUserB,
+				itemUserB: itemUserA,
+			}).then((result) => {
+				/*CHECK IF OTHER USER ALREADY AGREED*/
+				if (!result.length) {
+					TransactionModel.create({
+						itemUserA: itemUserA,
+						itemUserB: itemUserB,
+						itMatches: false,
+					})
+						.then((response) => {
+							res.status(200).json(response);
+						})
+						.catch((err) => {
+							res.status(500).json({
+								error: "Something went wrong",
+								message: err,
+							});
+						});
+				} else {
+					/*WHEN BOTH USER AGREED*/
+					TransactionModel.findOneAndUpdate(
+						{ _id: result[0]._id },
+						{ itMatches: true }
+					).then((response) => {
+						res.status(200).json(response);
+					});
+				}
+			});
+		}
 	});
 });
 
